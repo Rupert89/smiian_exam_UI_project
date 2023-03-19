1c4b37c2c26146fa418e7c998317444144acf6d1
smiian <smiyana.1989@gmail.com>
2023-03-19 18:51:33 +0200

    08_23_TC#4 full but with page opening problem

--- src/test/java/homePageTest/HomePageTest.java
+++ src/test/java/homePageTest/HomePageTest.java
@@ -12,15 +12,12 @@ public class HomePageTest extends BaseTest {
                 .openLoginPage()
                 .fillingLoginFormWithValidCred()
              .getHeaderElement().checkIsElementContainText(TestData.NAME_OF_USER)
-//             .getHeaderElement().checkIsElementContainText("Вихід [ testerArt ]")
                 .getHeaderElement().clickOnProfileButton()
                 .clickOnEditYourProfileButton()
                 .enterDataIntoOccupationField(TestData.OCCUPATION_TEXT)
-//                .enterDataIntoOccupationField("test engineer")
                 .clickOnSubmitButton()
                 .getHeaderElement().clickOnProfileButton()
                 .checkIsElementContainText(TestData.OCCUPATION_TEXT)
-//                .checkIsElementContainText("test engineer")
         ;
     }
 
